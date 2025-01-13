89cc613915ac65c9a9a05653ec72548733a6797b
jasonmayyo <141422819+JasonMayo23587572@users.noreply.github.com>
2025-01-13 15:55:53 +0200

    Initial Commit

---
+++ diewithoutregrets/diewithoutregretsApp.swift
@@ -0,0 +1,17 @@
+//
+//  diewithoutregretsApp.swift
+//  diewithoutregrets
+//
+//  Created by Jason Mayo on 2025/01/13.
+//
+
+import SwiftUI
+
+@main
+struct diewithoutregretsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
