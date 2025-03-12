3c3ca425ecde5d2195600ac033b4f98d92883e11
jasonmayyo <141422819+JasonMayo23587572@users.noreply.github.com>
2025-03-12 20:09:05 +0200

    Added Lock and Unlock View

---
+++ diewithoutregrets/Resources/UnlockView.swift
@@ -0,0 +1,18 @@
+//
+//  UnlockView.swift
+//  diewithoutregrets
+//
+//  Created by Jason Mayo on 2025/03/12.
+//
+
+import SwiftUI
+
+struct UnlockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UnlockView()
+}
